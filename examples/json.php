d8896e7c4cec796ca2d7c36f07994af47617cf0c
khepin - Sébastien Armand <khepin@gmail.com>
2016-04-07 23:14:19 -0700

    more example

--- examples/json.php
+++ examples/json.php
@@ -9,6 +9,36 @@ function stringify($tree) {
 $parse = \Khepin\Azay\Parsers::from_grammar(file_get_contents(__DIR__.'/json.bnf'));
 
 $input = '[1,2,3, [4,5,6]]';
-// $input = '{ "h": "bob", "i":"bloup" }';
+$input = '{ "h": "bob", "i":"bloup" }';
 $output = $parse($input);
 var_dump(stringify($output));
+
+// Compiling
+use Khepin\Azay\t;
+
+function identity($thing) {
+    return $thing;
+}
+
+$transforms = new \splObjectStorage;
+$transforms[t::n('number')] = 'identity';
+$transforms[t::n('string')] = 'identity';
+$transforms[t::n('bool')] = 'identity';
+$transforms[t::n('array')] = function() {
+    return func_get_args();
+};
+$transforms[t::n('key')] = 'identity';
+$transforms[t::n('value')] = 'identity';
+$transforms[t::n('pair')] = function($key, $value) {
+    return [$key, $value];
+};
+$transforms[t::n('object')] = function() {
+    $items = func_get_args();
+    $obj = [];
+    foreach($items as $item) {
+        $obj[$item[0]] = $item[1];
+    }
+    return (object) $obj;
+};
+
+var_dump(\Khepin\Azay\Tree::transform($output, $transforms));
